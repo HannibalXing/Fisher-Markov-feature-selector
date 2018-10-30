b2456e7916646b90f96ae18467a5a87ca19f4e4c
XIN <hannibalxing@gmail.com>
2018-10-30 17:37:11 -0400

    Update runit.py

--- runit.py
+++ runit.py
@@ -11,7 +11,6 @@ from MFS import Alg
 The scipt is based on the Alg.1 of paper ''Fast selecting optimal feature subset for multi-class classification
 with applications to high dimensional data'' Q. Cheng, H. Zhou, and J. Cheng, submitted to IEEE Trans. PAMI.
 
-
 The input data should be .csv file, dataset contains no header index
 and the first column is the labels, the rest column is the features of dataset.
 
